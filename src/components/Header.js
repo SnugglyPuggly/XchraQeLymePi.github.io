ac3a5f7610c349725cf0eda75ee4ad9cff7fd42e
XchraQeLymePi <69987770+XchraQeLymePi@users.noreply.github.com>
2025-03-29 10:19:42 -0700

    baller

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="top-bar"></div>
+      <div className="header">
+        <div className="header-left">
+          
+        </div>
+        
+
+        {/* Right Side: Button */}
+        <button className="header-button">agoaho</button>
+      </div>
+    </header>
+  );
+}
